f655c5b158b36525349d90f4f4c5ea78089a7db5
RicardoQ7 <richard1921rq@gmail.com>
2023-05-24 09:08:46 -0500

    reactfase5g29
    Organizando las paginas de la fase 4

---
+++ public/js/respuesta2.js
@@ -0,0 +1,21 @@
+function procesarRespuestas(){   // mi función 
+    let total = 5               // total de preguntas
+    let puntos = 0;             // inicializo los puntos en cero
+
+    let myForm = document.forms["evaluacionSelectores"];                 // creo una variable que apunte al formulario (se llama evaluacion_Selectores)
+    let respuestasCorrectas = ["a", "d", "a", "b", "c"];      // en esta variable, alojo las respuestas correctas de las 5 preguntas
+
+    for (let i =1; i <= total; i++){                // el ciclo for para recorrer los elementos del arreglo, donde la variable 'i' inicia en 1 y va hasta <n, sumando de 1 en las respuestas.
+        if (myForm["r" + i].value == null ||        // si el cuestionario es respondido de forma incompleta
+            myForm["r" + i].value == '') {          // o el cuestionario no lo respondieron 
+            alert('Favor responder todas las preguntas'); // mensaje 
+            return false;
+        } else {
+            if (myForm["r" + i].value === respuestasCorrectas[i -1])
+                puntos++;
+        }
+    }
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML='Obtuviste '+puntos+' puntos de ' + total + ' posibles ';
+    return false;
+}
